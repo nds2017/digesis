44624f85d2f964e27c1813fc76383f7122a11a78
zfrankitoz21 <fk.franko.soto@gmail.com>
2016-12-12 08:52:49 -0500

    fix

--- encuesta/js/ui-front.js
+++ encuesta/js/ui-front.js
@@ -345,8 +345,6 @@ if($(".cont-login.thanks").length!==0){
 				t.find("li a").removeClass("active");
 				var s=$(this);
 				if ( s.hasClass('icon-encuestar') ) {
-					var $_GET = <?php echo json_encode($_GET); ?>;
-					alert($_GET);
 					location.href = "../index.php/encuestas/indicaciones/" + $("#test-sid").val();
 				}
 				s.addClass("active");
