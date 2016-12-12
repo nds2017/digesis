6362b204ad3f147360403771e5b042d9b19834d7
zfrankitoz21 <fk.franko.soto@gmail.com>
2016-12-12 09:23:18 -0500

    fix

--- application/views/gracias.php
+++ application/views/gracias.php
@@ -24,7 +24,6 @@
 			 
 			</div>
 		</div>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js" type="text/javascript"></script>
 		<script src="<?=base_url()?>encuesta/js/ui-front.js" type="text/javascript"></script>
 	</body>
 </html>
