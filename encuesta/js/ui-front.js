74644f098b0403605276a8187db3a44992933ac0
zfrankitoz21 <fk.franko.soto@gmail.com>
2016-12-14 08:39:55 -0500

    fix

--- encuesta/js/ui-front.js
+++ encuesta/js/ui-front.js
@@ -349,9 +349,8 @@ if($(".cont-login.thanks").length!==0){
 		$(".ui-popup-options-contenido .icon-cerrar").click(function(event) {
 			//$(this).parent().parent().parent().parent().hide();
 			event.preventDefault();
-			
+			console.log($(this).parent().parent());
 			//setTimeout(hidepop, 500);
-			
 			$( "#datepicker" ).datepicker('setDate', null);
 		});	
 		$(".ui-popup-options-contenido .icon-client").click(function(event) {
