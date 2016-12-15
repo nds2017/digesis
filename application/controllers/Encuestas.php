9b473f3aeacbf92f4032dc42ce6285eaa4c372c1
zfrankitoz21 <fk.franko.soto@gmail.com>
2016-12-15 00:55:58 -0500

    fix

--- application/controllers/Encuestas.php
+++ application/controllers/Encuestas.php
@@ -39,7 +39,7 @@ class Encuestas extends CI_Controller {
 
 	public function pendiente($sid = null) {
 		if ( isset($sid) && !empty($sid) ) {
-			return 'hola';
+			echo 'hola';
 			$form = array('id' => $sid, 'estadoid' => 3);
 			$this->msolicitudes->solicitudes_update($form, $sid);
 			redirect('encuestas?dni=' . $_GET['dni']);
