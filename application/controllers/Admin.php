18555b608389abed9c4abe9a3e42fe3085ab03ad
zfrankitoz21 <fk.franko.soto@gmail.com>
2016-12-21 13:02:42 -0500

    fix

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -64,7 +64,7 @@ $mensaje = '<html>
    $cabeceras = 'MIME-Version: 1.0' . "\r\n";
    $cabeceras .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
    $cabeceras .= 'From: Digetel <digetelservicio@digetel.pe>' . "\r\n";
-   mail($_POST['search'], "Recuperar contraseña", $mensaje, $cabeceras);
+   var_dump(mail($_POST['search'], "Recuperar contraseña", $mensaje, $cabeceras));
 
 			echo '¡Revisa Tu Correo!';
 		}
