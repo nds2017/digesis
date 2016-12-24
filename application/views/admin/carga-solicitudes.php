ea085b3df33bfe8382a284e566f702023d838386
zfrankitoz21 <fk.franko.soto@gmail.com>
2016-12-24 03:34:38 -0500

    fix

--- application/views/admin/carga-solicitudes.php
+++ application/views/admin/carga-solicitudes.php
@@ -28,7 +28,7 @@
 
 			<h1>Solicitudes Cargadas Desde <?=date('d-m-Y')?></h1>
 			<?php
-							$this->db->query("DELETE FROM `solicitudestecnicos` WHERE sid = '1122'");
+							$this->db->query("DELETE FROM `solicitudestecnicos` WHERE sid = '2147483647'");
 				$this->db->query("ALTER TABLE `solicitudestecnicos` CHANGE `sid` `sid` INT(11) NOT NULL;");
 				//$this->db->query("DELETE FROM `solicitudestecnicos` WHERE sid = 't75r65'");
 			/*$this->db->query("CREATE TABLE IF NOT EXISTS `tblreseteopass` (
