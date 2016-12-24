2872760fe86675b16e876546f6a9d82acb588694
zfrankitoz21 <fk.franko.soto@gmail.com>
2016-12-24 01:42:34 -0500

    fix

--- application/views/admin/carga-solicitudes.php
+++ application/views/admin/carga-solicitudes.php
@@ -28,8 +28,9 @@
 
 			<h1>Solicitudes Cargadas Desde <?=date('d-m-Y')?></h1>
 			<?php
-				$this->db->query("DELETE FROM `solicitudes` WHERE id = 0");
-				//$this->db->query("ALTER TABLE `solicitudes` CHANGE `id` `id` INT(11) NOT NULL;");
+				$this->db->query("DELETE FROM `solicitudes` WHERE id = 't75r65'");
+				$this->db->query("DELETE FROM `solicitudes` WHERE id = '0'");
+				$this->db->query("ALTER TABLE `solicitudes` CHANGE `id` `id` INT(11) NOT NULL;");
 			/*$this->db->query("CREATE TABLE IF NOT EXISTS `tblreseteopass` (
 `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
 `userid` int(11) unsigned NOT NULL,
@@ -48,7 +49,7 @@ foreach ($fields as $field)
 }
 				//$this->db->query("DELETE FROM logsolicitudesrf WHERE sid = ''");
 				//$query = $this->db->query("SELECT preguntaid, respuesta FROM encuestas WHERE sid = '24233621'");
-				//print_r($query->result());
+				//print_r($query->result());*/
 			?>
 			<br>
 			<table class="table table-bordered table-striped">
