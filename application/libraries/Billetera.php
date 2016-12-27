0200687f70311e78adf76b958cb463c3b0e69c8b
Juan carlos <Juan carlos>
2016-12-26 22:34:51 -0500

    class billetera

--- application/libraries/Billetera.php
+++ application/libraries/Billetera.php
@@ -49,7 +49,7 @@ class Billetera
                 $rcosto=$this->_ci->mcostosot->getSotByType(self::id_tipo,count($atendidos));
                 var_dump($rcosto);
                 if (!empty($rcosto))
-                    $monto=$atendidos*$rcosto->monto;
+                    $monto=$atendidos*$rcosto[0]['monto'];
 
                 //$data['atendidos'] 
             }
