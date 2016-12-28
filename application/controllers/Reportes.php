e75efd13fcae58b580a823bdb1554aada51a5c6d
zfrankitoz21 <fk.franko.soto@gmail.com>
2016-12-28 05:18:42 -0500

    fix

--- application/controllers/Reportes.php
+++ application/controllers/Reportes.php
@@ -28,35 +28,32 @@ class Reportes extends CI_Controller {
 	public function tecnico_encuestas($tid = null) {
 		if ( is_numeric($tid) && ( $tid != 0 ) ) {
 			$data = $this->mreportes->tecnico_getEncuestas($tid);
-			print '<pre>';
-			print_r($data);
-			print '</pre>';
-			//$this->load->view('admin/reportes/tecnico_encuestas', $data);
+			$data['tecnicos'] = $this->mtecnicos->tecnicos_combo();
+			$data['tid'] = $tid;
+			$this->load->view('admin/reportes/tecnico_encuestas', $data);
 		}
 		else
 			redirect('reportes');
 	}
 
 	public function supervisor_encuestas($supid = null) {
-		if ( is_numeric($supid) && ( $supid != 0 ) ) {
+		if ( is_numeric($supid) && ( $supid != 0 ) ) 
+			$data['supervisores'] = $this->msupervisores->supervisores_combo();
+			$data['tecnicos'] = $this->mtecnicos->tecnicos_combo();
+			$data['supid'] = $supid;
 			$tecnicos = $this->mtecnicos->tecnicos_bySupervisor($supid);
 			$data = $this->mreportes->supervisor_getEncuestas($tecnicos, $supid);
-			print '<pre>';
-			print_r($data);
-			print '</pre>';
-			//$this->load->view('admin/reportes/tecnico_encuestas', $data);
+			$this->load->view('admin/reportes/supervisor_encuestas', $data);
 		}
 		else
 			redirect('reportes');	
 	}
 
 	public function jefe_encuestas($jefeid = null) {
-		$rows = array();
-		$data['header'] = $this->load->view('admin/menu/header', array('active' => 'encuestas' ));
-		$data['jefes'] = $this->mjefes->jefes_combo();
-		$data['supervisores'] = $this->msupervisores->supervisores_combo();
-		$rows['promedio'] = 0;
 		if ( is_numeric($jefeid) && ( $jefeid != 0 ) ) {
+			$data['header'] = $this->load->view('admin/menu/header', array('active' => 'encuestas' ));
+			$data['jefes'] = $this->mjefes->jefes_combo();
+			$data['supervisores'] = $this->msupervisores->supervisores_combo();
 			$supervisores = $this->msupervisores->supervisores_combo($jefeid);
 			$data['data'] = $this->mreportes->jefe_getEncuestas($supervisores, $jefeid);
 			$data['jefeid'] = $jefeid;
