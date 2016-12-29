ab42f8c3dc20097d8f63ab846f96ff5edfb8de15
Juan carlos <Juan carlos>
2016-12-28 22:17:43 -0500

    class billetera

--- application/models/Minsidencia.php
+++ application/models/Minsidencia.php
@@ -1,5 +1,6 @@
 <?php
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
 class Minsidencia extends CI_Model
 {
 	public function __construct()
@@ -7,17 +8,5 @@ class Minsidencia extends CI_Model
 		parent::__construct();
 
 	}
-
-	public function getPenalidadesById($codigo=null){		
-
-		 $rows = array();
-	     $query = $this->db->get_where('penalidades', array('codigo' => $codigo));
-	     if( $query->num_rows() > 0 ) {
-	     	$r=$query->result();
-	     	return $r[0]->monto;		     
-			}
-		
-	}
-
 
 }
