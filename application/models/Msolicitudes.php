4ff1fe9f3c812989b7ff44df267a0dd708376aba
Juan carlos <Juan carlos>
2016-12-28 23:21:12 -0500

    class billetera

--- application/models/Msolicitudes.php
+++ application/models/Msolicitudes.php
@@ -497,7 +497,8 @@ public function solicitudesByMonthCount($tid=null){
 	month(FROM_UNIXTIME(solicitudes.fecha_instalacion))=date('m')
 	group by solicitudes.fecha_instalacion";
 	$query= $this->db->query($sql);
-	echo $this->db->get_compiled_select();
+	echo $this->db->last_query();
+	exit;
 	return $query->result_array();
 
 }
