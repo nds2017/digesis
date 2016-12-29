3da2922ad24321c688d5d2d5cdbb3ce02fcd76d5
Juan carlos <Juan carlos>
2016-12-28 22:15:30 -0500

    class billetera

--- application/models/Minsidencia.php
+++ application/models/Minsidencia.php
@@ -1,27 +1,21 @@
 <?php
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class Mincidencia extends CI_Model
+class Minsidencia extends CI_Model
 {
-	
 	public function __construct()
 	{
 		parent::__construct();
 
 	}
-	public function getIncidenciasById($idsot=null){		
 
-		$rows = array();
-		$this->db->select('i.*');
-		$this->db->from('incidencias i');
-		$this->db->where('month(FROM_UNIXTIME(i.fecha_incidencia))=',intval(date('m')));
-		$this->db->where('i.sid=',$idsot);
-		$query = $this->db->get();  
+	public function getPenalidadesById($codigo=null){		
+
+		 $rows = array();
+	     $query = $this->db->get_where('penalidades', array('codigo' => $codigo));
 	     if( $query->num_rows() > 0 ) {
-	     	foreach ( $query->result() as $key => $row ) {
-				$rows[$row->id] = $row;
+	     	$r=$query->result();
+	     	return $r[0]->monto;		     
 			}
-		}
 		
 	}
 
