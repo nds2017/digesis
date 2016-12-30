20de1b51401747b1c1b854507316637addc1dbd9
Juan carlos <Juan carlos>
2016-12-29 19:15:36 -0500

    class billetera

--- application/models/Msolicitudes.php
+++ application/models/Msolicitudes.php
@@ -53,7 +53,8 @@ class Msolicitudes extends CI_Model
 		$this->db->order_by("s.fecha_instalacion, s.id");
 
 		$query = $this->db->get();
-		var_dump($query->result());
+		echo $this->db->last_query();
+		exit;
 
 		if ( $query->num_rows() > 0 ) {
 			foreach ( $query->result() as $key => $row ) {
