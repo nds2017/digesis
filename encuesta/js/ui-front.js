d97a01c9b18cc492091c62d10cb466d982c7022e
Juan carlos <Juan carlos>
2016-12-31 17:49:09 -0500

    class billetera

--- encuesta/js/ui-front.js
+++ encuesta/js/ui-front.js
@@ -96,7 +96,7 @@ $( "#datepicker2" ).datepicker({
 
 $( ".cont_billetera" ).on( "click", function() {
     event.preventDefault();
-    	//$(".ui-popup-options").show();
+    	$(".ui-popup-options").show();
 		$( ".client-box" ).addClass('display-none');
 		$(".ui-popup-options-list li a").removeClass("active");
 		$(".billetera").show();
@@ -104,6 +104,7 @@ $( ".cont_billetera" ).on( "click", function() {
 		$(".client").hide();
 		$(".schedule").hide();
 		$(".reject").hide();
+		$(".test").hide();
 		$(".schedule .datetime").show();
 		$(".schedule .calendar").show();
 		//$(".test").find("#test-sid").val(sid);
