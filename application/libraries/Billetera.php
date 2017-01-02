8c75db4792600e21400c59a1f716cf4d0cae7851
Juan carlos <Juan carlos>
2017-01-01 19:05:21 -0500

    class billetera

--- application/libraries/Billetera.php
+++ application/libraries/Billetera.php
@@ -236,6 +236,7 @@ restore_error_handler();
                         $c=0;                        
                         $monto_desc_rf = $this->_ci->mpenalidades->getPenalidadesById(self::CODIGO_RF);   
                             foreach ($this->atendidos as $key => $value) {
+                                echo $key.'---';
                                 $rf=$this->_ci->msolicitudes->solicitudesByIdAndDate($key);
                                 if ($rf)
                                  $c++;
