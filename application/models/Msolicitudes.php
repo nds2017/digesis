c7bb9583afae089dfb804f1269238f635f056ef5
Juan carlos <Juan carlos>
2017-01-01 20:16:14 -0500

    class billetera

--- application/models/Msolicitudes.php
+++ application/models/Msolicitudes.php
@@ -62,6 +62,7 @@ class Msolicitudes extends CI_Model
 		$query = $this->db->get();
 		echo $this->db->last_query();
 		echo date('Y-m-d 00:00:00');
+		echo strtotime(date('Y-m-d 00:00:00'));
 		exit;
 		/*if ($date) {			
 		echo $this->db->last_query();
