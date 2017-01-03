3350e7994b631a1e259aee23d548f8d1cb5f09b1
Juan carlos <Juan carlos>
2017-01-02 23:56:17 -0500

    billetera

--- application/views/admin/menu/header.php
+++ application/views/admin/menu/header.php
@@ -31,10 +31,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 				<dt><a <?=($active=='solicitudesload')?'class="active"':''?>href="<?=base_url()?>index.php/solicitudes/carga"><i class="fa fa-star"></i> Cargar Solicitudes</a></dt>
 				<dt><a <?=($active=='asignartecnicos')?'class="active"':''?>href="<?=base_url()?>index.php/solicitudes/listatecnicos"><i class="fa fa-star"></i> Soporte de Servicio</a></dt>
 				<dt><a <?=($active=='listarf')?'class="active"':''?>href="<?=base_url()?>index.php/solicitudes/listarf"><i class="fa fa-star"></i> Registro Fotográfico</a></dt>
+
+				<dt><a <?=($active=='asistencia')?'class="active"':''?>href="<?=base_url()?>index.php/asistencia"><i class="fa fa-star"></i> Asistencia de Técnicos</a></dt>
+
 				<dt><a <?=($active=='usuarios')?'class="active"':''?>href="<?=base_url()?>index.php/usuarios"><i class="fa fa-star"></i> Roles</a></dt>
+
 					<dd><a <?=($active=='usuarios')?'class="active"':''?> href="<?=base_url()?>index.php/usuarios"><i class="fa fa-ticket"></i> Todas Los Usuarios </a></dd>
 					<dd><a <?=($active=='usuariosadd')?'class="active"':''?> href="<?=base_url()?>index.php/usuarios/form"><i class="fa fa-ticket"></i> Crear Usuario </a></dd>
 					<dd><a <?=($active=='usuarioactivo')?'class="active"':''?> href="<?=base_url()?>index.php/usuarios/tuusuario/<?=$session->id?>"><i class="fa fa-ticket"></i> Tu Usuario </a></dd>
+
 				<dt><a <?=($active=='perfiles')?'class="active"':''?>href="<?=base_url()?>index.php/perfiles"><i class="fa fa-star"></i> Perfiles</a></dt>
 					<dd><a <?=($active=='perfiles')?'class="active"':''?>href="<?=base_url()?>index.php/perfiles"><i class="fa fa-ticket"></i> Todas Los Perfiles </a></dd>
 					<dd><a <?=($active=='jefes')?'class="active"':''?>href="<?=base_url()?>index.php/jefes/form"><i class="fa fa-ticket"></i> Crear Jefe </a></dd>
