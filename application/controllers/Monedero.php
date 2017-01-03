5fa6e2b12cdad6b7ec3f25619f77b2c06dec8552
Juan carlos <Juan carlos>
2017-01-02 23:59:04 -0500

    billetera

--- application/controllers/Monedero.php
+++ application/controllers/Monedero.php
@@ -66,7 +66,7 @@ class Monedero extends CI_Controller {
 			$detalle=$this->billetera->getdetalle_comision($params);			
 
 			$data['nombre'] = $r_tecnicos->nombres;						
-			$data['perfil']=($r_tecnicos->perfil==1)? 'perfil1':'perfil2';
+			$data['perfil']=($r_tecnicos->cargo==1)? 'perfil1':'perfil2';
 			$data['dni'] = $dni;			
 			$data['resumen'] = $resumen;
 			$data['detalle'] = $detalle;	
