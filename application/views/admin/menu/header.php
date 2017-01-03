7c766b5e1a975f5d0c0a3bc6bba40bf6aaff92e6
Juan carlos <Juan carlos>
2017-01-02 21:41:52 -0500

    billetera

--- application/views/admin/menu/header.php
+++ application/views/admin/menu/header.php
@@ -8,6 +8,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<link rel="stylesheet" type="text/css" href="<?=base_url()?>css/style.css">
 	<link rel="stylesheet" href="<?=base_url()?>css/font-awesome.min.css">
 	<script src="<?=base_url()?>encuesta/js/jquery-1.9.1.min.js"></script>
+	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+<script type="text/javascript">
+	  $( "#fecha" ).datepicker();
+  	  $( "#fecha" ).datepicker( "option", "dateFormat", "yy-mm-dd" );
+</script>
 </head>
 <body>
 <?php $session = get_session(); ?>
