2b3f4d0fbd4ac016c683fde10bbeff36729a120d
Juan carlos <Juan carlos>
2017-01-02 22:27:12 -0500

    billetera

--- application/views/admin/monedero_view.php
+++ application/views/admin/monedero_view.php
@@ -14,11 +14,10 @@
         <div class="form-group" style="margin-top: 15px;">
 
             <label style="display: inline; float: left; width:6%" for="ejemplo_email_1">Tecnico:</label>
-            <select required id="tecnico1id" name="tecnico1id" style="float: left;width: 20%">
-                <option value="">-Seleccione-</option>
-                <?php foreach ( @$tecnicos as $key => $tecnico1 ) { ?>
-                <option <?=(@$dni==$key ? 'selected' : '')?>  value="<?=$key?>"><?=$tecnico1?></option>
-                <?php } ?>
+            <select required id="tecnico1id" name="tecnico1id" style="float: left;width: 20%">                
+                <option <?php echo (empty($perfil))? 'selected':'' ?> value="all">Todos los Tecnicos </option>
+                <option <?php echo ($perfil==1)? 'selected':'' ?> value="1">Peril1</option>
+                <option <?php echo ($perfil==2)? 'selected':'' ?> value="2">Perfil2 </option>                
               </select>
 
           <label style="display: inline; float: left; width: 6%; margin-left:10px" for="ejemplo_email_1">Fecha:</label>          
