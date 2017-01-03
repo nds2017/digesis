3a8aa26fb9332852c249f795fe3d2eff73764927
Juan carlos <Juan carlos>
2017-01-02 19:52:28 -0500

    billetera

--- application/views/admin/asistencia_view.php
+++ application/views/admin/asistencia_view.php
@@ -7,46 +7,7 @@
       </div>
       <br>
       <fieldset class="search">
-        <legend></legend>
-        <form id="form" method="post" action="<?=base_url()?>index.php/solicitudes/lista/<?=@$estadoid?>">
-          <nav class="top_menu">
-            <ul>
-              <li <?=(@$estadoid==0)?'class="active"':''?>><a href="<?=base_url()?>index.php/solicitudes/lista/0">Todos (<?=$cantidades[0]?>) &nbsp;&nbsp;|</a></li>
-              <?php foreach ( $estados as $id => $estado ) { ?>
-              <li <?=(@$estadoid==$id)?'class="active"':''?>><a href="<?=base_url()?>index.php/solicitudes/lista/<?=$id?>"><?=$estado?> (<?=isset($cantidades[$id]) ? $cantidades[$id] : 0?>) &nbsp;&nbsp;|</a></li>
-              <?php } ?>
-            </ul>
-          </nav>
-
-          <input type="hidden" id="url" value="<?=base_url()?>index.php/solicitudes"/>
-          Departamento :
-          <select id="dptoid">
-            <option value="0">-Seleccione-</option>
-            <?php foreach ($departamentos as $id => $departamento) { ?>
-            <option <?=(@$departamentoid==$id ? 'selected' : '')?> value=<?=$id?>><?=$departamento?></option>
-            <?php } ?>
-          </select>
-          Provincia
-          <select id="provinciaid" name="provinciaid">
-            <?php if ( @$distritoid ) { ?>
-            <?php foreach ($provincias as $id => $provincia) { ?>
-            <option <?=(@$provinciaid==$id ? 'selected' : '')?> value=<?=$id?>><?=$provincia?></option>
-            <?php } ?>
-            <?php } ?>
-          </select>
-          Distrito:
-          <select name="distritoid" id="distritoid">
-            <?php if ( @$distritoid ) { ?>
-            <?php foreach ($distritos as $id => $distrito) { ?>
-            <option <?=(@$distritoid==$id ? 'selected' : '')?> value=<?=$id?>><?=$distrito?></option>
-            <?php } ?>
-            <?php } ?>
-          </select>
-          <input type="submit" class="btnsearch" value="Filtrar"/>
-          <br>
-          N° SOT: <input type="text" size="10" name="solicitudid" value="<?=@$solicitudid?>"/>
-          <input type="submit" class="btnsearch" value="Buscar"/>
-        </form>
+        <legend></legend>        
       </fieldset>
       <br>
       <table class="table table-bordered table-striped">      
