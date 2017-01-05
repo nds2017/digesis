a7a1adb56d6a440ca4250123f9741bb39c507110
Juan carlos <Juan carlos>
2017-01-04 22:53:24 -0500

    billetera

--- application/views/list-solicitudes-supervisor.php
+++ application/views/list-solicitudes-supervisor.php
@@ -68,7 +68,7 @@ window.location.href=url;
 		<div class="body_w">
 			<div class="list-solicitud">
 				<div class="cont_time-s">				
-					<h4>Fecha: <input style="color: #333" type="text" id="fecha" value="<?=date('Y-m-d')?>" /></h4>
+					<h4>Fecha: <input style="color: #333" type="text" id="fecha" value="<?=@$fecha?>" /></h4>
 					<h4><em>Bienvenido <?=@$nom_supervisor?></em><br>
 					<a href="<?=base_url()?>">Cerrar Sesión</a></h4>
 				</div>
