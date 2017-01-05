f7e1b1c51b88007a74edb3767f09912f362967bd
Juan carlos <Juan carlos>
2017-01-04 22:12:38 -0500

    billetera

--- application/views/list-solicitudes-supervisor.php
+++ application/views/list-solicitudes-supervisor.php
@@ -7,6 +7,8 @@
 		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 
 
+		<link href="<?=base_url()?>encuesta/css/style.css?v=11" rel="stylesheet" type="text/css">
+
 		<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
     
     <!-- Load jQuery JS -->
