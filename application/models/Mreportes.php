563e2c3416a31348d861c2864e1dc4c942173dca
zfrankitoz21 <fk.franko.soto@gmail.com>
2017-01-05 02:34:56 -0500

    reportes

--- application/models/Mreportes.php
+++ application/models/Mreportes.php
@@ -137,7 +137,6 @@ class Mreportes extends CI_Model
 		$rows = array();
 		foreach ( $jefes as $id => $jefe ) {
 			$supervisores = $this->msupervisores->supervisores_byJefe($id);
-			var_dump($supervisores); die();
 			if ( count($supervisores) ) {
 				$rows[$id] = $this->mreportes->jefes_getTotalSolicitudes($supervisores);
 			}
