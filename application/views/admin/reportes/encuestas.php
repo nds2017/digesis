ed73aeda4e45c5cabc7185dd5ef2bb9f2bfaeda2
zfrankitoz21 <fk.franko.soto@gmail.com>
2017-01-12 17:23:42 -0500

    reportes

--- application/views/admin/reportes/encuestas.php
+++ application/views/admin/reportes/encuestas.php
@@ -73,7 +73,7 @@
 						<tr>
 							<td><strong>-</strong></td>
 							<td><strong>-</strong></td>
-							<td><strong><?=$data_t['nombre']?></strong></td>
+							<td><strong><?=$data_t['nombres']?></strong></td>
 							<td><strong><?=isset($data_t['promedio'])?$data_t['promedio']:'-'?></strong></td>
 							<td><a title="Ver Detalle" href="<?=base_url()?>index.php/reportes/tecnico_encuestas/<?=$tid?>"><img src="<?=base_url()?>img/editar.png"></a></td>
 						</tr>
