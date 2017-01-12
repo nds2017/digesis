265d54c34f9b8fe87ea555ff06e2b9728d568b60
Juan carlos <Juan carlos>
2017-01-11 22:25:41 -0500

    billetera

--- application/models/Madmin.php
+++ application/models/Madmin.php
@@ -32,6 +32,12 @@ public function supervisores_login($dni = null) {
 	}
 
 
+public function jefes_login($dni = null) {
+		return $this->db->get_where('jefes', array('dni' => $dni, 'publish' => 1))->row();
+	}
+
+
+
 	public function admin_inactivo($user = null) {
 		return $this->db->get_where('usuarios', array('user' => $user, 'publish' => 0))->row();
 	}
