ddc10cf370d77260fe735ec691074f9721d56566
Juan carlos <Juan carlos>
2017-01-11 23:16:54 -0500

    billetera

--- application/controllers/Encuestas.php
+++ application/controllers/Encuestas.php
@@ -266,15 +266,23 @@ foreach ($r_jefes as $key => $value_jefes) {
 
 	$r_supervisor=$this->msupervisores->supervisores_byJefe($value_jefes->id);
 
+	$data['jefe']['nom_jefe']=$value_jefes->nombres.' '.$value_jefes->apellidos;
+
+	$acumulador=array('nuevos'=>0,'atendidos'=>0,'pendientes'=>0,'reprogramados'=>0,'rechazados'=>0);
+
 	foreach ($r_supervisor as $key_sup => $value_sup) {
 		
+	$key_sup=$value_sup->id;	
 	$data['jefe']['supervisor'][$key_sup]['nom_supervisor']=$value_sup->nombres.' '.$value_sup->apellidos;
 
 
 	$r_tecnicos=$this->mtecnicos->tecnicos_bySupervisor2($value_sup->id);
 
-
-$acumulador=array('nuevos'=>0,'atendidos'=>0,'pendientes'=>0,'reprogramados'=>0,'rechazados'=>0);
+	$sum_sup[$key_sup]['nuevos']=0;
+	$sum_sup[$key_sup]['atendidos']=0;
+	$sum_sup[$key_sup]['pendientes']=0;
+	$sum_sup[$key_sup]['reprogramados']=0;
+	$sum_sup[$key_sup]['rechazados']=0;
 
 	foreach ($r_tecnicos as $key_tecnico => $value)	{
 		$datat = $this->mtecnicos->tecnicobyDNI($value->dni);
@@ -286,26 +294,35 @@ $data['jefe']['supervisor'][$key_sup][$key_tecnico]['tecnico']=$datat->nombres;
 
 $data['jefe']['supervisor'][$key_sup][$key_tecnico]['nuevos']=$this->msolicitudes->solicitudes_encuestas($tid, 1, false,$fecha);
 
+
 $acumulador['nuevos']=intval($acumulador['nuevos'])+count($data['jefe']['supervisor'][$key_sup][$key_tecnico]['nuevos']);
+
+$sum_sup[$key_sup]['nuevos']=intval($sum_sup[$key_sup]['nuevos'])+count($data['jefe']['supervisor'][$key_sup][$key_tecnico]['nuevos']);
 
 
 $data['jefe']['supervisor'][$key_sup][$key_tecnico]['atendidos']=$this->msolicitudes->solicitudes_encuestas($tid, 2, false,$fecha);
 
 	$acumulador['atendidos']=intval($acumulador['atendidos']) + count($data['jefe']['supervisor'][$key_sup][$key_tecnico]['atendidos']);
 
+	$sum_sup[$key_sup]['atendidos']=intval($sum_sup[$key_sup]['atendidos'])+count($data['jefe']['supervisor'][$key_sup][$key_tecnico]['atendidos']);
+
 	$data['jefe']['supervisor'][$key_sup][$key_tecnico]['pendientes']= $this->msolicitudes->solicitudes_encuestas($tid, 3);
 
 	$acumulador['pendientes']=intval($acumulador['pendientes']) +count($data['jefe']['supervisor'][$key_sup][$key_tecnico]['pendientes']);
 
-			
-			$data['jefe']['supervisor'][$key_sup][$key_tecnico]['reprogramados']= $this->msolicitudes->solicitudes_encuestas($tid, 4, false,$fecha);
+	$sum_sup[$key_sup]['pendientes']=intval($sum_sup[$key_sup]['pendientes']) +count($data['jefe']['supervisor'][$key_sup][$key_tecnico]['pendientes']);
+
+	$data['jefe']['supervisor'][$key_sup][$key_tecnico]['reprogramados']= $this->msolicitudes->solicitudes_encuestas($tid, 4, false,$fecha);
 
 			$acumulador['reprogramados']=intval($acumulador['reprogramados']) + count($data['jefe']['supervisor'][$key_sup][$key_tecnico]['reprogramados']);
 
+	$sum_sup[$key_sup]['reprogramados']=intval($sum_sup[$key_sup]['reprogramados'])+ count($data['jefe']['supervisor'][$key_sup][$key_tecnico]['reprogramados']);
 
 			$data['jefe']['supervisor'][$key_sup][$key_tecnico]['rechazados']=$this->msolicitudes->solicitudes_encuestas($tid, 5, false,$fecha);	
 
-			$acumulador['rechazados']=intval($acumulador['rechazados']) + count($data['jefe']['supervisor'][$key_sup][$key_tecnico]['rechazados']);
+	$acumulador['rechazados']=intval($acumulador['rechazados']) + count($data['jefe']['supervisor'][$key_sup][$key_tecnico]['rechazados']);
+
+	$sum_sup[$key_sup]['rechazados']=intval($sum_sup[$key_sup]['rechazados'])+ count($data['jefe']['supervisor'][$key_sup][$key_tecnico]['rechazados']);
 
 			$data['jefe']['supervisor'][$key_sup][$key_tecnico]['sinfotos']=$this->msolicitudes->solicitudesrf_encuestas($tid);
 
@@ -315,13 +332,10 @@ $data['jefe']['supervisor'][$key_sup][$key_tecnico]['atendidos']=$this->msolicit
 	}
 
 }
-}
-				
-		$data['acumulador']=$acumulador;
-		print_r($data);
-		exit();
-			
-		$this->load->view('list-solicitudes-supervisor', $data);
+}				
+		$data['sum_sup']=$sum_sup;
+		$data['acumulador']=$acumulador;					
+		$this->load->view('list-solicitudes-jefes', $data);
 	}
 			else
 				redirect('welcome');
---
+++ application/views/list-solicitudes-jefe.php
@@ -0,0 +1,207 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Lista de Solicitudes Jefes</title>
+		
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+
+		<link href="<?=base_url()?>encuesta/css/style.css?v=11" rel="stylesheet" type="text/css">
+
+		<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+    
+    <!-- Load jQuery JS -->
+    <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+
+    <!-- Load jQuery UI Main JS  -->
+    <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+									
+
+    
+<script type="text/javascript">
+
+$(function() {
+
+function getParameterByName(name, url) {
+    if (!url) {
+      url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+$.datepicker.regional['es'] = {
+ closeText: 'Cerrar',
+ prevText: '< Ant',
+ nextText: 'Sig >',
+ currentText: 'Hoy',
+ monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+ monthNamesShort: ['Ene','Feb','Mar','Abr', 'May','Jun','Jul','Ago','Sep', 'Oct','Nov','Dic'],
+ dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+ dayNamesShort: ['Dom','Lun','Mar','Mié','Juv','Vie','Sáb'],
+ dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sá'],
+ weekHeader: 'Sm',
+ dateFormat: 'dd/mm/yy',
+ firstDay: 1,
+ isRTL: false,
+ showMonthAfterYear: false,
+ yearSuffix: ''
+ };
+ $.datepicker.setDefaults($.datepicker.regional['es']);
+ $( "#fecha" ).datepicker({
+    //changeMonth: true,//this option for allowing user 
+    //changeYear: true ,
+    dateFormat:'yy-mm-dd',
+	onSelect: function () {
+		var dni = getParameterByName('dni');				  
+var url ='/index.php/encuestas/jefe?dni='+dni+'&fecha='+$.datepicker.formatDate("yy-mm-dd", $(this).datepicker('getDate'));
+
+
+window.location.href=url;
+            //selectedDate = $.datepicker.formatDate("yy-mm-dd", $(this).datepicker('getDate'));
+        }
+    });
+    });         
+	
+</script>
+	</head>
+
+
+	<body>	
+	
+	<div class="wrapper">		
+			<header>
+			<a href="<?=base_url()?>index.php/encuestas/jefe?dni=<?=$_GET['dni']?>">
+			<div class="cont_logo">
+				<img src="<?=base_url()?>encuesta/img/logo.png"/>
+			</div>
+			</a>			
+			<div style="clear: both;"></div>
+			</header>
+		
+		<div class="body_w">
+			<div class="list-solicitud">
+				<div class="cont_time-s">	
+					<h4>Fecha: <input style="color: #333" type="text" id="fecha" value="<?=@$fecha?>" /></h4>
+					<h4><em>Bienvenido <?=@$jefe['nom_jefe']?></em><br>
+					<a href="<?=base_url()?>">Cerrar Sesión</a></h4>
+				</div>
+				<div class="estados">
+					<div class="total_e">
+						Total Programados: <?php echo $acumulador['nuevos'] + $acumulador['reprogramados'] + $acumulador['atendidos'] + $acumulador['pendientes'] + $acumulador['rechazados']						
+						?>
+					</div>
+					<div class="cont_e">
+						<div class="cont-1">
+							<h4>
+								Validado
+							</h4>
+							<h3>
+								<?=$acumulador['atendidos']?>
+							</h3>
+						</div>
+						<div class="cont-1 cont2">
+							<h4>
+								Pendiente de Validar
+							</h4>
+							<h3>
+								<?=$acumulador['pendientes']?>
+							</h3>
+						</div>
+					</div>
+					<div class="cont_e">
+						<div class="cont-1 cont3">
+							<h4>
+								Reprogramado
+							</h4>
+							<h3>
+								<?=$acumulador['reprogramados']?>
+							</h3>
+						</div>
+						<div class="cont-1 cont4">
+							<h4>
+							   Rechazado
+							</h4>
+							<h3>
+								<?=$acumulador['rechazados']?>
+							</h3>
+						</div>
+					</div>
+				</div>
+				<div class="container-list" id="tab-ui">
+				<!--
+					<ul class="ui-tabs-links tab-links">
+						<li class="active">
+							<a href="#" class="check">
+								<span></span>	
+							</a>
+						</li>
+					
+						<li>
+							<a href="#"  class="alert">
+								<span>(<?=count($pendientes)?>)</span> 
+							</a>
+						</li>
+						<li>
+							<a href="#"  class="photo">
+								<span>(<?=count($sinfotos)?>)</span>
+							</a>
+						</li>                        
+						
+					</ul>
+					-->
+					<div class="wrap-ui-container-tab">
+						<div class="tab-content-group ui-container-tab">
+							<div class="ui-tab-item active tab-content">
+								<div class="tab-title">RESUMEN POR SUPERVISOR</div>
+		           
+
+				<table id="tbmonedero" class="detalle-billetera">
+					<thead>
+						<tr>		
+							<th>Supervisor</th>
+							<th>SOT Atendidos</th>
+							<th>SOT pendientes</th>		
+							<th>SOT reprogramados</th>	  <th>SOT rechazados</th>	
+						</tr>
+					</thead>
+					<tbody>
+	<?php 					
+	foreach($jefe['supervisor'] as $key=>$value):
+	?>
+	<tr>	
+	<td data-label="Supervisor">
+	<?php echo $value['nom_supervisor']; ?></td>
+	<td data-label="SOT Atendidos">
+	<?php echo $sum_sup[$key]['atendidos'] ?></td>
+	<td data-label="SOT pendientes">
+	<?php echo $sum_sup[$key]['pendientes'] ?></td>
+	<td data-label="SOT reprogramados">
+	<?php echo $sum_sup[$key]['reprogramados'] ?></td>
+	<td data-label="SOT rechazados">
+	<?php echo $sum_sup[$key]['rechazados'] ?></td>
+						
+	</tr>
+	<?php 
+		endforeach;
+	?>	
+					</tbody>					  
+				</table>
+
+
+																
+							</div>
+													
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		</div>
+	</body>
+</html>
