91cb72860759a8f2c954214276419c064a064055
Juan carlos <Juan carlos>
2017-01-16 07:44:17 -0500

    billetera

--- application/controllers/Asistencia.php
+++ application/controllers/Asistencia.php
@@ -14,7 +14,7 @@ class Asistencia extends CI_Controller {
 			  
 	 public function index()
 	 {	 	
-	 	securityAccess(array(1));
+	 	securityAccess(array(1,2));
 	 	date_default_timezone_set('America/Lima');
 		$data['header'] = $this->load->view('admin/menu/header', array('active' => 'asistencia' ));
 
