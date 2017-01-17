722070fcc432dcc107e1729bb049cd47472a7522
Juan carlos <Juan carlos>
2017-01-17 07:39:59 -0500

    billetera

--- application/views/admin/solicitudes_asignar.php
+++ application/views/admin/solicitudes_asignar.php
@@ -33,7 +33,7 @@ $(document).ready(function() {
 </script>
 
 			<div class="list-mod-panel">
-				<h1 style="float: left;"> <?=(@$data)?'Editar Solicitud' : 'Agregar Solicitud'?> &nbsp;&nbsp;</h1>
+				<h1 style="float: left;"> <?=(@$data)?'Editar Solicitud' : 'Asignar Solicitud'?> &nbsp;&nbsp;</h1>
 				<h2><a href="<?=base_url()?>index.php/solicitudes/lista">Regresar a Lista de Solicitudes</a></h2>
 			</div>
 			<br>
