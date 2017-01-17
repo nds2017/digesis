9ff1292276bbb67e8f70b65997252fc6e585ec99
zfrankitoz21 <fk.franko.soto@gmail.com>
2017-01-17 02:28:23 -0500

    fix

--- application/views/admin/tecnicosedit.php
+++ application/views/admin/tecnicosedit.php
@@ -22,7 +22,7 @@
 					<td>Apellidos : </td><td><input pattern="[/\s/ÑñA-Za-z]{1,30}" title="Solo Letras" type="text" required maxlength="30" name="apellidos" value="<?=@$data->apellidos?>"></td>
 				</tr>
 				<tr>
-					<td>DNI : </td><td><input class="solo-numero" type="text" pattern=".{8}" required name="dni" value="<?=@$data->dni?>"></td>
+					<td>DNI : </td><td><input class="solo-numero" title="8 Dígitos" type="text" pattern=".{8}" required name="dni" value="<?=@$data->dni?>"></td>
 				</tr>
 				<tr>
 					<td>Correo : </td><td><input type="email" required maxlength="40" size="40" name="email" value="<?=@$data->email?>"></td>
