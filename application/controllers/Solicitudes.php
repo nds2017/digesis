47fd95fd402907b41257c7e963b0685a1ae5456a
zfrankitoz21 <fk.franko.soto@gmail.com>
2017-01-17 08:46:55 -0500

    fix

--- application/controllers/Solicitudes.php
+++ application/controllers/Solicitudes.php
@@ -412,7 +412,7 @@ class Solicitudes extends CI_Controller {
 		$this->msolicitudes->solicitudes_addtecnicos($formdata);
 		$formdata = array(
 			'id' => $id,
-			'fecha_instalacion' => $this->input->post('fecha_instalacion'),
+			'fecha_instalacion' => $this->input->post('fecha_instalacion') ? strtotime($fecha) : strtotime('now'),
 			'horario' => $this->input->post('horarioid'),
 			'hora' => $this->input->post('hora'),
 			'modtime' => strtotime("now")
