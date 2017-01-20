d551595bc47acc2ae332470ef1c1dea10e30c1db
Juan carlos <Juan carlos>
2017-01-19 23:19:28 -0500

    billetera

--- application/controllers/Encuestas.php
+++ application/controllers/Encuestas.php
@@ -156,9 +156,10 @@ class Encuestas extends CI_Controller {
 		//date_default_timezone_set("GMT");
 		//date_default_timezone_set('UTC');
 		//setlocale(LC_ALL,"es_ES");
-		$lenguage = 'es_ES.UTF-8';
+		$lenguage = 'spanish';
 		putenv("LANG=$lenguage");
 		setlocale(LC_ALL, $lenguage);
+
 		if (isset($_GET['dni']) && (!empty($_GET['dni']))){
 			$resumen=$this->billetera_resumen($_GET['dni']);	
 			$detalle=$this->billetera_detalle($_GET['dni']);			
