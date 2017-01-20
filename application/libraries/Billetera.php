53c852fd7d649412b12d19ae9a814e2d56b90147
Juan carlos <Juan carlos>
2017-01-19 20:57:12 -0500

    billetera

--- application/libraries/Billetera.php
+++ application/libraries/Billetera.php
@@ -227,9 +227,7 @@ restore_error_handler();
     echo "<br/>";
     echo $desc_mes_insidencia;
     echo "<br/>";
-    exit();
-    
-
+    exit();    
     $r_data['monto']=($comision_mes_sot + $comision_mes_eficiencia - ($desc_mes_inasistencia + $desc_mes_rf_no_validada +$desc_mes_insidencia));
 
     
