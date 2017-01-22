b1d1b3e86a4a27a041e5dc805e7ced313b5774cb
Juan carlos <Juan carlos>
2017-01-21 22:14:00 -0500

    billetera

--- application/views/admin/carga-servicios.php
+++ application/views/admin/carga-servicios.php
@@ -24,8 +24,8 @@
 		$('#myTable tr td').on( 'click', 'a.serv_delete', function () {
 			var id=$(this).attr('data-id');
 			var me=$(this);
-			$(me).parent().parent().parent().fadeTo(800, 0, function () {       $(me).remove(); });
-			
+			$(me).parent().parent().parent().fadeTo(800, 0, function () {       $(this).remove(); });
+
 			$.ajax({      				
 				data: {id : id},
 				url:   '/index.php/servicios/delete',
