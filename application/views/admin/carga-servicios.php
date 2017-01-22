59a92828e61386a0ef008f1f3c179aa973791554
Juan carlos <Juan carlos>
2017-01-21 19:02:41 -0500

    billetera

--- application/views/admin/carga-servicios.php
+++ application/views/admin/carga-servicios.php
@@ -24,7 +24,7 @@
 			<hr style="border: 1px blue solid">			
 			<br>
 
-		<div class="row" style="border: solid 1px #b3b3b3;margin-left: 10px; margin-right: 10px;padding-top:10px">
+		<div class="row" style="border: solid 1px #b3b3b3;margin-left: 10px; margin-right: 10px;padding-top:10px; margin-bottom: 15px;">
 			  <div class="col-xs-2">
 			    <input type="text" class="form-control" placeholder="servicio">
 			  </div>
