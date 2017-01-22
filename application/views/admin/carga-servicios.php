c9aeceb6b18d9b582c18f9406a958f1fcbff0c3c
Juan carlos <Juan carlos>
2017-01-22 18:25:19 -0500

    reporte

--- application/views/admin/carga-servicios.php
+++ application/views/admin/carga-servicios.php
@@ -98,7 +98,7 @@
 			  <div class="col-xs-2">
 			    <input type="text" id="txt_servicio" class="form-control" placeholder="servicio">
 			  </div>
-			  <div class="col-xs-2">
+			  <div class="col-xs-3">
 				<select style="width: 100px" id="txt_categoria" placeholder="categoria">
 					<option>Categoria</option>
 					<?php foreach($categorias as $key=>$row){ ?>
