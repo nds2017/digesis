18b5a57552f47e87952ddca3deb93c4b0240cf00
Juan carlos <Juan carlos>
2017-01-21 19:28:08 -0500

    billetera

--- application/views/admin/menu/header.php
+++ application/views/admin/menu/header.php
@@ -5,9 +5,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="utf-8">	
 	<title>ATENCIÓN TÉCNICA CLARO</title>
-	<!--
+
 	<link rel="stylesheet" href="<?=base_url()?>css/bootstrap.css">
-	-->
+
 	<link rel="stylesheet" type="text/css" href="<?=base_url()?>css/style.css">
 	<link rel="stylesheet" href="<?=base_url()?>css/font-awesome.min.css">
 	<script src="<?=base_url()?>encuesta/js/jquery-1.9.1.min.js"></script>
