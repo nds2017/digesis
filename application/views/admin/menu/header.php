055e607cba234c5482f0b72f48b2435ac332b14d
Juan carlos <Juan carlos>
2017-01-21 19:30:09 -0500

    billetera

--- application/views/admin/menu/header.php
+++ application/views/admin/menu/header.php
@@ -22,6 +22,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	
 	<link rel="stylesheet" href="https://cdn.datatables.net/1.10.13/css/dataTables.bootstrap.min.css">
 	
+	<link rel="stylesheet" href="https://cdn.datatables.net/1.10.13/css/dataTables.bootstrap.min.css">
+	
 	<script src="//cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script>
 
 
