48da2b73ced10f97466191493133002ddcd0294e
Juan carlos <Juan carlos>
2017-01-22 20:12:11 -0500

    reporte

--- application/controllers/Solicitudes.php
+++ application/controllers/Solicitudes.php
@@ -137,13 +137,14 @@ class Solicitudes extends CI_Controller {
 			$r_sol_tec[$value->id]['tecnico1']=(($value->tecnico1!="")?$value->tecnico1:"sin asignar");
 			$r_sol_tec[$value->id]['tecnico2']=(($value->tecnico2!="")?$value->tecnico2:"sin asignar");
 
-	if ($value->tipotrabajoid==self::SERVICIO_INSTALACIONES)
+		$categoria=null;
+	if ($value->tiposervicioid==self::SERVICIO_INSTALACIONES)
 		$categoria="instalacion";
 
-	if ($value->tipotrabajoid==self::SERVICIO_MANTENIMIENTO)
+	if ($value->tiposervicioid==self::SERVICIO_MANTENIMIENTO)
 		$categoria='mantenimiento';
 
-	if ($value->tipotrabajoid==self::SERVICIO_POST_VENTA)
+	if ($value->tiposervicioid==self::SERVICIO_POST_VENTA)
 		$categoria='post instalacion';
 	
 	$r_sol_tec[$value->id]['tipotrabajo']=$this->mservicios->getByCategoria($categoria);
