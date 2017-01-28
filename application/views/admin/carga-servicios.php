63f83ace9e67bfe256e380de84863e83e88bc876
Juan carlos <Juan carlos>
2017-01-27 21:48:15 -0500

    admin

--- application/views/admin/carga-servicios.php
+++ application/views/admin/carga-servicios.php
@@ -99,7 +99,7 @@
 			    <input type="text" id="txt_servicio" class="form-control" placeholder="servicio">
 			  </div>
 			  <div class="col-xs-3">
-				<select style="width: 150px" id="txt_categoria" placeholder="categoria">
+				<select style="width: 130px" id="txt_categoria" placeholder="categoria">
 					<option>Categoria</option>
 					<?php foreach($categorias as $key=>$row){ ?>
 <option value="<?php echo trim($row->id) ?>">
