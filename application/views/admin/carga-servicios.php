876b611e572146f5dff2259c1ef770c1cc3e27a9
Juan carlos <Juan carlos>
2017-01-28 09:21:40 -0500

    admin

--- application/views/admin/carga-servicios.php
+++ application/views/admin/carga-servicios.php
@@ -130,7 +130,7 @@
 <div class="col-xs-1"> 
 	<span style="display:inline; width:100px;margin-right:10px; ">Filtrar por:</span>
 </div>
-			<div class="col-xs-2">		
+			<div class="col-xs-2" style="margin-left: 15px;">		
 				<select style="width: 100px" id="txt_categoria" placeholder="categoria">
 					<option>Categoria</option>
 					<?php foreach($categorias as $key=>$row){ ?>
