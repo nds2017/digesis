5494889abc1fc4b45c97ed08315e8a65bcd62b6e
Juan carlos <Juan carlos>
2017-01-28 18:24:45 -0500

    admin

--- application/views/admin/servicios_editar.php
+++ application/views/admin/servicios_editar.php
@@ -13,12 +13,12 @@ echo form_open_multipart('servicios/update/'.$servicios[0]->id);
 
 			<table class="table table-bordered table-striped">
 				<tr>
-					<td>Servicio : </td><td><input title="Solo Letras" type="text" required maxlength="30" autofocus="autofocus" name="nombres" value="<?=@$servicios[0]->descripcion?>"></td>
+					<td>Servicio : </td><td><input title="Solo Letras" type="text" required maxlength="30" autofocus="autofocus" name="descripcion" value="<?=@$servicios[0]->descripcion?>"></td>
 				</tr>
 				<tr>
 				<td>Categoria</td>
 				<td>
-					<select id="sel_categoria" placeholder="categoria">
+					<select id="sel_categoria" name="categoria" placeholder="categoria">
 					<option>Categoria</option>
 					<?php $sel=null;?>
 					<?php foreach($categorias as $key=>$row){
@@ -35,10 +35,10 @@ echo form_open_multipart('servicios/update/'.$servicios[0]->id);
 					</td>
 				</tr>
 				<tr>
-					<td>Motivo : </td><td><input title="Solo Letras" type="text" maxlength="30" name="apellidos" value="<?=@$servicios[0]->motivos?>"></td>
+					<td>Motivo : </td><td><input title="Solo Letras" type="text" maxlength="30" name="motivos" value="<?=@$servicios[0]->motivos?>"></td>
 				</tr>
 				<tr>
-					<td>Foto : </td><td><input class="solo-numero" title="Sólo 2 Dígitos" pattern=".{2}" type="number" required name="dni" value="<?=@$servicios[0]->fotos?>"></td>
+					<td>Foto : </td><td><input class="solo-numero" title="Sólo 2 Dígitos" pattern=".{2}" type="number" required name="fotos" value="<?=@$servicios[0]->fotos?>"></td>
 				</tr>
 				
 			</table>
